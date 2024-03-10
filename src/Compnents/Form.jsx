5e99f38d4441363c605c57731d16ef53b302cc8a
NmtDev404 <nadimmahmudtusher@gmail.com>
2024-03-11 01:41:38 +0600

    new update

--- src/Compnents/Form.jsx
+++ src/Compnents/Form.jsx
@@ -1,105 +1,117 @@
-
-import React from 'react'
-import {useForm} from 'react-hook-form'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Form = () => {
-
-    const {register,handleSubmit}= useForm()
-    const formSubmit = (data)=> {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const formSubmit = (data) => {
         console.log(data);
-    }
-  return (
-    <div>
-       <div className="max-w-md mx-auto m-4 bg-white p-8 rounded shadow-md">
-      <h2 className="text-2xl font-bold mb-6">Sign Up</h2>
-      <form onSubmit={handleSubmit(formSubmit)}>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
-            First Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="firstName"
-            type="text"
-            placeholder="First Name"
-            {...register('first_name')}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
-            Last Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="lastName"
-            type="text"
-            placeholder="Last Name"
-            {...register('last_name')}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
-            Phone Number
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="phone"
-            type="text"
-            placeholder="Phone Number"
-            {...register('phone')}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Email"
-            {...register('email')}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Password"
-            {...register('password')}
-          />
-        </div>
+    };
 
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-           Confirm Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="confirm password"
-            type="password"
-            placeholder="confirm password"
-            {...register('confirm_password')}
-          />
-        </div>
+    return (
+        <div>
+            <div className="max-w-md mx-auto m-4 bg-white p-8 rounded shadow-md">
+                <h2 className="text-2xl font-bold mb-6">Sign Up</h2>
+                <form onSubmit={handleSubmit(formSubmit)}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
+                            First Name
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="firstName"
+                            type="text"
+                            placeholder="First Name"
+                            required
+                            {...register('firstName', { minLength: { value: 3, message:'Minimum length is 3 characters' }, maxLength: { value: 10, message:'Maximum length is 10 characters' } })}
+                        />
+                      
+                            <p className="text-red-500">{errors.firstName?.message}</p>
+                        
+                        
+                    </div>
 
-       
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Sign Up
-          </button>
-        </div>
-      </form>
-    </div>
-    </div>
-  )
-}
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lasttName">
+                            Last Name
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="lastName"
+                            type="text"
+                            placeholder="Last Name"
+                            required
+                            {...register('lastName', { minLength: { value: 3, message:'Minimum length is 3 characters' }, maxLength: { value: 10, message:'Maximum length is 10 characters' } })}
+                        />
+                      
+                            <p className="text-red-500">{errors.lastName?.message}</p>
+                        
+                        
+                    </div>
 
-export default Form
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="...peer shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="email"
+                            type="text"
+                            placeholder="Your Email"
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                            required
+                            {...register('email', { minLength: { value: 3, message:'Minimum length is 3 characters' }, maxLength: { value: 10, message:'Maximum length is 10 characters' } })}
+                        />
+                      
+                            <p className=" mt-2 text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block ">{errors.email?.message}</p>
+                        
+                        
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="Password"
+                            
+                            required
+                            {...register('password', { minLength: { value: 7, message:'Minimum length is 7 characters' }, maxLength: { value: 20, message:'Maximum length is 20 characters' } })}
+                        />
+                      
+                            <p className="text-red-500">{errors.password?.message}</p>
+                        
+                        
+                    </div>
+
+                    
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirm_password">
+                        Confirm password
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="confirm_password"
+                            type="password"
+                            placeholder="Confirm password"
+                            required
+                            {...register('password', { minLength: { value: 7, message:'Minimum length is 7 characters' }, maxLength: { value: 20, message:'Maximum length is 20 characters' } })}
+                        />
+                      
+                            <p className="text-red-500">{errors.password?.message}</p>
+                        
+                        
+                    </div>
+                  
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Sign Up
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Form;
